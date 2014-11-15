63d1c11666ed72c8742fcded45aacb622d624196
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-11-15 15:51:56 -0800

    Added scrum app. Up to "designing the API"

---
+++ scrum/board/migrations/0001_initial.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Sprint',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('name', models.CharField(blank=True, default='', max_length=100)),
+                ('description', models.TextField(blank=True, default='')),
+                ('end', models.DateField(unique=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=100)),
+                ('description', models.TextField(blank=True, default='')),
+                ('status', models.SmallIntegerField(default=1, choices=[(1, 'Not Started'), (2, 'In Progress'), (3, 'Testing'), (4, 'Done')])),
+                ('order', models.SmallIntegerField(default=0)),
+                ('started', models.DateField(null=True, blank=True)),
+                ('due', models.DateField(null=True, blank=True)),
+                ('completed', models.DateField(null=True, blank=True)),
+                ('assigned', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True, null=True)),
+                ('sprint', models.ForeignKey(to='board.Sprint', blank=True, null=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
